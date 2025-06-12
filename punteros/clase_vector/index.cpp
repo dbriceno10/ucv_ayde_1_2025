98b864a772fe1c236e9c9f632ef1e3c2fc1cf9f5
Daniel Briceno <daniel.b100726@gmail.com>
2025-06-12 06:37:34 -0400

    class vector

--- punteros/clase_vector/index.cpp
+++ punteros/clase_vector/index.cpp
@@ -20,34 +20,183 @@ void initializeArr(int *&array, int n)
 
 void printArray(int *array, int length)
 {
-  cout << "[";
-  for (int i = 0; i < length; i++)
+  if (length <= 0)
   {
-    if (i == length - 1)
+    cout << "[]" << endl;
+  }
+  else
+  {
+    cout << "[";
+    for (int i = 0; i < length; i++)
     {
-      cout << array[i] << "]" << endl;
-    }
-    else
-    {
-      cout << array[i] << ", ";
+      if (i == length - 1)
+      {
+        cout << array[i] << "]" << endl;
+      }
+      else
+      {
+        cout << array[i] << ", ";
+      }
     }
   }
 }
 
+class IntVector
+{
+public:
+  int *data;
+  int size;
+
+public:
+  IntVector(int length)
+  {
+    data = new int[length];
+    size = length;
+  }
+  ~IntVector()
+  {
+    delete data;
+    size = 0;
+  }
+
+  int push(int element)
+  {
+    int *aux = new int[size + 1];
+    for (int i = 0; i < size; i++)
+    {
+      aux[i] = data[i];
+    }
+    aux[size] = element;
+    delete[] data;
+    data = aux;
+    size++;
+    return size;
+  }
+
+  int shift()
+  {
+    if (size - 1 < 0)
+    {
+      return 0;
+    }
+    else
+    {
+      int *aux = new int[size - 1];
+      int element = data[0];
+      for (int i = 1; i < size; i++)
+      {
+        aux[i - 1] = data[i];
+      }
+      delete[] data;
+      data = aux;
+      size--;
+      return element;
+    }
+  }
+
+  int pop()
+  {
+    if (size - 1 < 0)
+    {
+      return 0;
+    }
+    else
+    {
+      int *aux = new int[size - 1];
+      int element = data[size - 1];
+      for (int i = 0; i < size; i++)
+      {
+        aux[i] = data[i];
+      }
+      delete[] data;
+      aux[size] = element;
+      data = aux;
+      size--;
+      return element;
+    }
+  }
+
+  int unshift(int element)
+  {
+    int *aux = new int[size + 1];
+    for (int i = 1; i <= size; i++)
+    {
+      aux[i] = data[i - 1];
+    }
+    aux[0] = element;
+    delete[] data;
+    data = aux;
+    size++;
+    return size;
+  }
+};
+
 int main()
 {
-  srand(time(0)); //* Inicializa la semilla solo una vez, es para que en cada ejecucion random pueda generar numeros diferentes
-  int *array = nullptr, n = 0;
-  cout << "Tamanio del array" << endl;
-  cin >> n;
-  initializeArr(array, n);
-  int *copy = new int[n];
-  for (int i = 0; i < n; i++)
+  // srand(time(0)); //* Inicializa la semilla solo una vez, es para que en cada ejecucion random pueda generar numeros diferentes
+  // int *array = nullptr, n = 0;
+  // cout << "Tamanio del array" << endl;
+  // cin >> n;
+  // initializeArr(array, n);
+  // int *copy = new int[n];
+  // for (int i = 0; i < n; i++)
+  // {
+  //   copy[i] = array[i];
+  // }
+  // cout << "array" << endl;
+  // printArray(array, n);
+  // delete[] array;
+  IntVector vec(0);
+  int opcion;
+  do
   {
-    copy[i] = array[i];
-  }
-  cout << "array" << endl;
-  printArray(array, n);
-  delete[] array;
+    cout << "\n--- MENU ---\n";
+    cout << "1. Agregar un elemento al final de la lista\n";
+    cout << "2. Quitar el primer elemento de la lista\n";
+    cout << "3. Quitar el ultimo elemento de la lista\n";
+    cout << "4. Agregar un elemento al princio de la lista\n";
+    cout << "9. Salir\n";
+    cout << "Seleccione una opción: ";
+    cin >> opcion;
+
+    if (opcion == 1)
+    {
+      int valor;
+      cout << "Ingrese el valor a agregar: ";
+      cin >> valor;
+      vec.push(valor);
+      cout << "Valor agregado.\n";
+      printArray(vec.data, vec.size);
+    }
+    else if (opcion == 2)
+    {
+      int valor = vec.shift();
+      cout << "El valor obtenido es " << valor << endl;
+      printArray(vec.data, vec.size);
+    }
+    else if (opcion == 3)
+    {
+      int valor = vec.pop();
+      cout << "El valor obtenido es " << valor << endl;
+      printArray(vec.data, vec.size);
+    }
+    if (opcion == 4)
+    {
+      int valor;
+      cout << "Ingrese el valor a agregar: ";
+      cin >> valor;
+      vec.unshift(valor);
+      cout << "Valor agregado.\n";
+      printArray(vec.data, vec.size);
+    }
+    else if (opcion == 9)
+    {
+      cout << "Saliendo...\n";
+    }
+    else
+    {
+      cout << "Opción no válida.\n";
+    }
+  } while (opcion != 9);
   return 0;
 }
