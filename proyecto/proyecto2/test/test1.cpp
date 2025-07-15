0961fdf3f72400b791f5edabdee1c60614116345
Daniel Briceno <daniel.b100726@gmail.com>
2025-07-15 16:05:21 -0400

    advances

--- proyecto/proyecto2/test/test1.cpp
+++ proyecto/proyecto2/test/test1.cpp
@@ -73,22 +73,104 @@ string readInput()
   return dna;
 }
 
+int factorial(int n)
+{
+  if (n <= 1)
+    return 1;
+  return n * factorial(n - 1);
+}
+
+bool include(string *array, string string, int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    if (array[i] == string)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+// imprimir premutacion
+void imprimirPermutacion(char arr[], int n, string *array, int &size)
+{
+  string perm = "";
+  for (int i = 0; i < n; i++)
+  {
+    perm += arr[i];
+  }
+  if (size == 0)
+  {
+    array[size] = perm;
+    size++;
+  }
+  else
+  {
+    if (!include(array, perm, size))
+    {
+      array[size] = perm;
+      size++;
+    }
+  }
+}
+
+void generarPermutaciones(char *arr, int n, int index, string *array, int &size)
+{
+  // cout << "index: " << index << endl;
+  // cout << "n: " << n << endl;
+  // imprimirPermutacion(arr, n);
+  if (index == n)
+  {
+    // cout << "index == n" << endl;
+    // Imprimir la permutación
+    imprimirPermutacion(arr, n, array, size);
+    // cout << "suma: " << calcularSuma(arr, n) << endl;
+    return;
+  }
+
+  for (int i = index; i < n; i++)
+  {
+    swap(arr[index], arr[i]);                             // Intercambiar
+    generarPermutaciones(arr, n, index + 1, array, size); // Llamada recursiva
+    swap(arr[index], arr[i]);                             // Deshacer el intercambio
+  }
+}
+
 int main(int argc, char const *argv[])
 {
-  /* code */
   string dna = readInput();
   cout << "DNA string: " << dna << endl;
-  int size = dna.size();
-  char *c = new char[size + 1];
-  for (int i = 0; i < size; i++)
+  int n = dna.size();
+  char *c = new char[n + 1];
+  for (int i = 0; i < n; i++)
   {
     c[i] = dna[i];
   }
-  for (int i = 0; i < size; i++)
+  for (int i = 0; i < n; i++)
   {
     cout << c[i] << " ";
   }
-  delete[] c;
-  c = nullptr;
+
+  int combinaciones = factorial(n);
+
+  string *array = new string[combinaciones];
+  int size = 0;
+
+  cout << "\nTodas las permutaciones posibles: " << combinaciones << endl;
+  generarPermutaciones(c, n, 0, array, size);
+
+  cout << "Total de permutaciones validas: " << size << endl;
+  cout << "Permutaciones:" << endl;
+  for (int i = 0; i < size; i++)
+  {
+    cout << array[i] << endl;
+  }
+
+  delete[] c;      // Free the allocated memory
+  c = nullptr;     // Avoid dangling pointer
+  delete[] array;  // Free the allocated memory for permutations
+  array = nullptr; // Avoid dangling pointer
+
   return 0;
 }
